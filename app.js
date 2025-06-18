e4472386d716876221a56ef3ea51ebaf0dbc270f
vitoriacorreaa <vitoria.correa.santos02@escola.pr.gov.br>
2025-06-18 12:11:50 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,87 @@
+function criaCartao(categoria, pergunta, resposta) {
+Adicionar comentário
+Mais ações
+
+
+    let container = document.getElementById('container')
+
+
+    let cartao = document.createElement('article')
+
+
+    cartao.className = 'cartao'
+
+
+
+
+
+    cartao.innerHTML = `
+
+
+    <div class="cartao__conteudo">
+
+
+    <h3>${categoria}</h3>
+
+
+    <div class="cartao__conteudo__pergunta">
+
+
+        <p>${pergunta}</p>
+
+
+    </div>
+
+
+    <div class="cartao__conteudo__resposta">
+
+
+        <p>${resposta}</p>
+
+
+    </div>
+
+
+    </div>
+
+
+    `
+
+
+
+
+
+    let respostaEstaVisivel = false
+
+
+
+
+
+    function viraCartao() {
+
+
+        respostaEstaVisivel = !respostaEstaVisivel
+
+
+        cartao.classList.toggle('active', respostaEstaVisivel)
+
+
+    }
+
+
+    cartao.addEventListener('click', viraCartao)
+
+
+
+
+
+
+
+
+    container.appendChild(cartao)
+
+
+
+
+
+}
